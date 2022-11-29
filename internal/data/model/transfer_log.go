c5375d52351adb089e07e770157cc95599ef6a1c
William9923 <williamong1400@gmail.com>
2022-11-29 15:30:31 +0700

    feat: initialize basic model for the system

---
+++ internal/data/model/transfer_log.go
@@ -0,0 +1,16 @@
+package model
+
+type TransferLog struct {
+	ID        uint64
+	UserID1   uint64
+	UserID2   uint64
+	Amount    int32
+	CreatedAt uint64
+	Status    byte
+}
+
+const (
+	INVALID = iota
+	VALID
+	DELETED
+)
---
+++ internal/data/model/user.go
@@ -0,0 +1,9 @@
+package model
+
+type User struct {
+	UserID    uint64
+	Name      string
+	Balance   int32
+	CreatedAt uint64
+	UpdatedAt uint64
+}

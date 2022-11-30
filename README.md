edafe8c0c4cfd6fa63edd4f9dcc16675f3f737d5
William9923 <williamong1400@gmail.com>
2022-11-30 11:31:45 +0700

    docs: improve documentation

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# clean-transaction
-A sample project showcasing how to use transaction with clean &amp; readable code
+# Clean Hexagonal Architecture SQL Transaction
+A sample cli project showcasing how to use transaction with clean &amp; readable code in hexagonal architecture
 
-## How to use:
-- Transfer from user 1 to user 2 for 20000
+## 🚀 Quick start
+1. Clone the repository
+```bash
+git clone git@github.com:William9923/clean-architecture.git
 ```
+2. Setup mysql infrastructure (use docker!)
+```bash
+sh ./scripts/mysql/run-local
+```
+3. Import the Postman collection in `.api` folder
+
+4. Run the Proof of Concept Code (Transfer from user 1 to user 2 for 20000)
+```bash
 go run cmd/cli/main.go -from 1 -to 2 -amount 20000
 ```
+5. To stop the infrastructure 
+```bash
+sh ./scripts/mysql/stop-local
+```
+
+## ❌ Prerequisites
+- Golang minimum v1.17 (https://golang.org/doc/install)
+- Go Modules (https://blog.golang.org/using-go-modules)
+
+## ❤️ Support
+If you feel that this repo have helped you provide more example on learning software engineering, then it is enough for me! Wanna contribute more? Please ⭐ this repo so other can see it too!
---
+++ scripts/mysql/init/01.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS local_transfer_proj_db;
+CREATE DATABASE local_transfer_proj_db; 
+USE local_transfer_proj_db;
+
+CREATE TABLE `user_tab` (
+    `user_id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `balance` INT UNSIGNED NOT NULL, 
+    `created_at` BIGINT UNSIGNED NOT NULL,
+    `updated_at` BIGINT UNSIGNED NULL,
+
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `transfer_log_tab` (
+    `transfer_id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    `user_id_1` BIGINT UNSIGNED NOT NULL,
+    `user_id_2` BIGINT UNSIGNED NOT NULL,
+    `amount` INT UNSIGNED NOT NULL, 
+    `created_at` BIGINT UNSIGNED NOT NULL,
+    `status` TINYINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (`transfer_id`),
+    KEY `idx_user_id_1` (`user_id_1`),
+    KEY `idx_user_id_2` (`user_id_2`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO local_transfer_proj_db.user_tab
+(name, balance, created_at, updated_at)
+VALUES('William', 100000, 1669718675585, 1669718675585);
+
+INSERT INTO local_transfer_proj_db.user_tab
+(name, balance, created_at, updated_at)
+VALUES('Pikachu', 50000, 1669718675585, 1669718675585);
